f8d66804f74d7d9e6fa862a9252a1ae71be8e046
Robles4 <164675622+Robles4@users.noreply.github.com>
2024-04-04 20:34:14 -1000

    added pygame
    added first video set of steps to creating snake game.

--- Week 12 Lab/Pygame.py
+++ Week 12 Lab/Pygame.py
@@ -1,10 +1,11 @@
 # import modules
 import pygame
+import random
 
 pygame.init()
 
 # create blank game window
-screen_width = 600
+screen_width: int = 600
 screen_height = 600
 
 # create game window
@@ -15,6 +16,9 @@ pygame.display.set_caption('Snake')
 cell_size = 10
 direction = 1  # 1 is up, 2 is right, 3 is down, 4 is left
 update_snake = 0
+food = [0, 0]
+new_food = True
+new_piece = [0, 0]
 
 # create snake
 snake_pos = [[int(screen_width / 2), int(screen_height / 2)],
@@ -26,11 +30,17 @@ snake_pos = [[int(screen_width / 2), int(screen_height / 2)],
 bg = (255, 200, 150)
 body_inner = (50, 175, 25)
 body_outer = (100, 100, 200)
+food_col = (200, 50, 50)
 red = (255, 0, 0)
+score = 0
 
 
 def draw_screen():
     screen.fill(bg)
+
+def draw_score():
+    score_txt = 'score: ' + str(score)
+
 
 
 # setup loop with exit event handler
@@ -52,6 +62,36 @@ while run:
                 direction = 3
             if event.key == pygame.K_LEFT and direction != 2:
                 direction = 4
+
+        # create food
+        if new_food == True:
+            new_food = False
+            food[0] = cell_size * random.randint(0, (screen_width // cell_size) - 1)
+            food[1] = cell_size * random.randint(0, (screen_height // cell_size) - 1)
+
+    # draw food
+    pygame.draw.rect(screen, food_col, (food[0], food[1], cell_size, cell_size))
+
+
+        #check if food has been eaten
+    if snake_pos[0] == food:
+        new_food = True
+        #create a new piece at the last point of the snake's tail
+        new_piece = list(snake_pos[-1])
+        if direction == 1:
+            new_piece[1] += cell_size
+        if direction == 3:
+            new_piece[1] -= cell_size
+        if direction == 2:
+            new_piece[0] -= cell_size
+        if direction == 4:
+            new_piece[0] += cell_size
+
+        #attached new piece to the end of the snake
+        snake_pos.append(new_piece)
+
+        #increase score
+        score += 1
 
     if update_snake > 99:
         update_snake = 0
